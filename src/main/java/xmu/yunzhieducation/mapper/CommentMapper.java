eae1ce7838c016ea8154c7fd127fd5fb6d6cb607
Emil123 <37292683+LiKseve@users.noreply.github.com>
2018-03-15 22:55:10 +0800

    Revert "Comment和Task，以及Task的select的一个例子"
    This reverts commit f113e2dec795928049e5f1d2e1abbf2ac0209a04.

--- src/main/java/xmu/yunzhieducation/mapper/CommentMapper.java
+++ src/main/java/xmu/yunzhieducation/mapper/CommentMapper.java
@@ -1,46 +1,4 @@
 package xmu.yunzhieducation.mapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Component;
-import xmu.yunzhieducation.entity.Comment;
-import xmu.yunzhieducation.entity.Topic;
 
-import java.math.BigInteger;
-import java.util.List;
-
-@Mapper
-@Component
 public interface CommentMapper {
-    /**
-     * 新增话题
-     */
-    void insertTopicByID(Topic topic);
-    /**
-     * 删除话题
-     */
-    void deleteTopicByID(@Param("user_id") BigInteger user_id,@Param("topic_id") BigInteger topic_id);
-    /**
-     * 话题中新增评论
-     */
-    void insertCommentByID(Comment comment);
-    /**
-     * 删除评论
-     */
-    void deleteCommentByID(@Param("user_id") BigInteger user_id,@Param("comment_id") BigInteger comment_id);
-    /**
-     * 根据ID选话题
-     */
-    List<Topic> selectTopicBybelongingID(@Param("belonging_id") BigInteger belonging_id,@Param("type") Integer type);
-    /**
-     * 找话题的全部评论
-     */
-    List<Comment> selectCommentBytopicID(@Param("topic_id") BigInteger topic_id);
-    /**
-     * 根据用户找话题
-     */
-    List<Topic> selectTopicByuserID(@Param("user_id") BigInteger user_id);
-    /**
-     * 根据用户找评论
-     */
-    List<Comment> selectCommentByuserID(@Param("user_id") BigInteger user_id);
 }
