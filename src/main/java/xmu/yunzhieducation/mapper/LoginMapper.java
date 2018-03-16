f404125e0684864e798c6aa67e5e3a495863ffbd
冯金刘 <707140010@qq.com>
2018-03-16 17:44:49 +0800

    Revert "Revert "user+school""
    This reverts commit b148abab82b6b2a88ce8394eb563f595c73dac55.

--- src/main/java/xmu/yunzhieducation/mapper/LoginMapper.java
+++ src/main/java/xmu/yunzhieducation/mapper/LoginMapper.java
@@ -1,8 +1,14 @@
 package xmu.yunzhieducation.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.type.BigIntegerTypeHandler;
 import org.springframework.stereotype.Component;
+import xmu.yunzhieducation.entity.Message;
 import xmu.yunzhieducation.entity.User;
+
+import java.math.BigInteger;
+import java.util.List;
+
 
 /**
  * School Mapper
@@ -16,4 +22,20 @@ public interface LoginMapper {
      *注册用户
      */
     void  insertUser(User user);
+    /**
+     *登陆用户
+     */
+    boolean  Login(@Param("account") String account,@Param("password") String password);
+    /**
+     *修改个人信息，包括密码
+     */
+    boolean  updateUserInfo(User user);
+    /**
+     *查看消息列表
+     */
+    List<Message>  listAllMessage(@Param("user_id") BigInteger user_id);
+    /**
+     *点击消息修改状态
+     */
+    boolean updateMessage(@Param("message_id") BigInteger message_id,@Param("index") int index);
 }
