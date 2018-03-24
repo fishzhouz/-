853e544312d7f1facdedf9ee4b4070185157ce7c
冯金刘 <707140010@qq.com>
2018-03-24 15:52:40 +0800

    comment +training service
    俩service 的接口和实现

---
+++ src/main/java/xmu/yunzhieducation/service/Impl/TrainingServiceImpl.java
@@ -0,0 +1,88 @@
+package xmu.yunzhieducation.service.Impl;
+
+/**
+ * Created by fengjinliu on 2018/3/24.
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import xmu.yunzhieducation.entity.*;
+import xmu.yunzhieducation.mapper.TrainingMapper;
+import xmu.yunzhieducation.mapper.LoginMapper;
+import xmu.yunzhieducation.mapper.CourseMapper;
+
+import xmu.yunzhieducation.service.TrainingService;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Service
+public class TrainingServiceImpl implements TrainingService{
+    @Autowired
+    private TrainingMapper trainingMapper;
+    @Autowired
+    private LoginMapper loginMapper;
+    @Autowired
+    private CourseMapper courseMapper;
+
+    @Override
+    public List<Trainging> GetTrainingByUserId(BigInteger user_id) {
+        List<Class_student> classList=courseMapper.selectClassstudentByStudentID(user_id);
+        List<BigInteger> class_idlist=null;
+        for (Class_student cs:classList
+             ) {
+            class_idlist.add(cs.getClass_id());
+        }
+        List<Trainging> traingingList=null;
+        for (BigInteger i:class_idlist) {
+            for (BigInteger j:class_idlist
+                 ) {
+                BigInteger k = courseMapper.selectCourseIDByClassID(j).getCourse_id();
+                for (Trainging t:trainingMapper.listTrainingByCourseId(k)
+                     ) {
+                    traingingList.add(t);
+                }
+            }
+        }
+        return traingingList;
+    }
+
+    @Override
+    public Trainging GetTrainingByTrainingId(BigInteger training_id) {
+        Trainging train=trainingMapper.listTrainingByTrainingId(training_id);
+        return train;
+    }
+
+    @Override
+    public Boolean UploadTrainingReport(Student_training st) {
+        trainingMapper.insertStudentTraining(st);
+        if(trainingMapper.listStudentTrainingByStudentIdAndTrainingId(st.getStudent_id()
+                ,st.gettraining_id()).equals(st))
+        return true;
+        else return false;
+    }
+
+    @Override
+    public Boolean UpdateTrainingReport(Student_training st) {
+        trainingMapper.updateStudentTrainingScoreByStudentIdAndTrainingId(st);
+        if(trainingMapper.listStudentTrainingByStudentIdAndTrainingId(st.getStudent_id(),
+                st.gettraining_id()).getScore()==st.getScore())
+            return true;
+        else return false;
+    }
+
+    @Override
+    public Student_training GetTraingingReport(BigInteger student_id,BigInteger training_id) {
+        return trainingMapper.listStudentTrainingByStudentIdAndTrainingId(student_id,training_id);
+
+    }
+
+    @Override
+    public Boolean InsertTraining(Trainging train) {
+        trainingMapper.insertTraining(train);
+        if(trainingMapper.listTrainingByTrainingId(train.getId()).equals(train))
+        return true;
+        else return false;
+    }
+}
+
---
+++ src/main/java/xmu/yunzhieducation/service/TrainingService.java
@@ -0,0 +1,43 @@
+package xmu.yunzhieducation.service;
+
+import org.springframework.stereotype.Component;
+import xmu.yunzhieducation.entity.Trainging;
+import xmu.yunzhieducation.entity.Student_training;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Component
+public interface TrainingService {
+    /**
+     * 获取当前用户下的所有实训
+     */
+    public List<Trainging> GetTrainingByUserId(BigInteger user_id);
+
+    /**
+     * 查看实训具体内容
+     */
+    public Trainging GetTrainingByTrainingId(BigInteger training_id);
+
+
+    /**
+     * 上传文件报告
+     */
+    public Boolean UploadTrainingReport(Student_training st);
+
+    /**
+     * 老师给学生报告打分
+     */
+    public Boolean UpdateTrainingReport(Student_training st);
+
+    /**
+     * 学生查看报告具体信息
+     */
+    public Student_training GetTraingingReport(BigInteger student_id,BigInteger training_id);
+
+    /**
+     * 管理员添加实训
+     */
+    public Boolean InsertTraining(Trainging train);
+
+}
