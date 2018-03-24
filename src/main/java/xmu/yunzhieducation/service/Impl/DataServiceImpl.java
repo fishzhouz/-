d050a9bdb18786e8852b82606f1a0a62965438df
HarryDongH <37304036+HarryDongH@users.noreply.github.com>
2018-03-24 14:06:55 +0800

    DataService接口记实现

---
+++ src/main/java/xmu/yunzhieducation/service/Impl/DataServiceImpl.java
@@ -0,0 +1,60 @@
+package xmu.yunzhieducation.service.Impl;
+
+import org.springframework.stereotype.Service;
+import xmu.yunzhieducation.entity.Ablity_file;
+import xmu.yunzhieducation.entity.User;
+import xmu.yunzhieducation.service.DataService;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class DataServiceImpl implements DataService{
+    /**
+     * 查看学生某门课的成绩
+     */
+    @Override
+    public int getGradeByCourseIDStudentID(BigInteger course_id,BigInteger student_id){
+        return 1;
+    }
+    /**
+     *老师或学校查看某个学生的能力档案
+     */
+    public List<Ablity_file> getAbilityfileByStudentID(BigInteger student_id){
+        List<Ablity_file> ablity_files=new ArrayList<>();
+        return ablity_files;
+    }
+    /**
+     * 学生查看自己的能力档案
+     */
+    public List<Ablity_file> getownAbilityfileByStudentID(BigInteger student_id){
+        List<Ablity_file> ablity_files=new ArrayList<>();
+        return ablity_files;
+    }
+    /**
+     * 根据学生ID和课程ID给某学生添加能力评价
+     */
+    public void insertAbilityfileByStudentID(BigInteger student_id, BigInteger course_id){
+
+    }
+    /**
+     * 老师获取某门课程下某班级的所有学生
+     */
+    public List<User> getAllStudentByClassID(BigInteger class_id){
+        List<User> users=new ArrayList<>();
+        return users;
+    }
+    /**
+     *获取某个实训下的参与度分析（包括应参与人数和已参与人数）
+     */
+    public double trainingparticipationByTrainingID(BigInteger training_id){
+        return 2;
+    }
+    /**
+     * 获得某个实训下的所有学生的得分
+     */
+    public List<Integer> getAllGradeByTrainingID(BigInteger training_id){
+        List<Integer> trainingID=new ArrayList<>();
+        return trainingID;
+    }
+}
