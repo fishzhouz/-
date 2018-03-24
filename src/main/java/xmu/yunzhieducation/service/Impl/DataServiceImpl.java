fe2593743d65b513d2d76d19ed09dc8db1e052e2
HarryDongH <37304036+HarryDongH@users.noreply.github.com>
2018-03-24 14:34:55 +0800

    DataService的实现

--- src/main/java/xmu/yunzhieducation/service/Impl/DataServiceImpl.java
+++ src/main/java/xmu/yunzhieducation/service/Impl/DataServiceImpl.java
@@ -10,6 +10,11 @@ import java.util.ArrayList;
 import java.util.List;
 @Service
 public class DataServiceImpl implements DataService{
+
+    
+
+
+
     /**
      * 查看学生某门课的成绩
      */
@@ -20,6 +25,7 @@ public class DataServiceImpl implements DataService{
     /**
      *老师或学校查看某个学生的能力档案
      */
+    @Override
     public List<Ablity_file> getAbilityfileByStudentID(BigInteger student_id){
         List<Ablity_file> ablity_files=new ArrayList<>();
         return ablity_files;
@@ -27,6 +33,7 @@ public class DataServiceImpl implements DataService{
     /**
      * 学生查看自己的能力档案
      */
+    @Override
     public List<Ablity_file> getownAbilityfileByStudentID(BigInteger student_id){
         List<Ablity_file> ablity_files=new ArrayList<>();
         return ablity_files;
@@ -34,12 +41,14 @@ public class DataServiceImpl implements DataService{
     /**
      * 根据学生ID和课程ID给某学生添加能力评价
      */
+    @Override
     public void insertAbilityfileByStudentID(BigInteger student_id, BigInteger course_id){
 
     }
     /**
      * 老师获取某门课程下某班级的所有学生
      */
+    @Override
     public List<User> getAllStudentByClassID(BigInteger class_id){
         List<User> users=new ArrayList<>();
         return users;
@@ -47,12 +56,14 @@ public class DataServiceImpl implements DataService{
     /**
      *获取某个实训下的参与度分析（包括应参与人数和已参与人数）
      */
+    @Override
     public double trainingparticipationByTrainingID(BigInteger training_id){
         return 2;
     }
     /**
      * 获得某个实训下的所有学生的得分
      */
+    @Override
     public List<Integer> getAllGradeByTrainingID(BigInteger training_id){
         List<Integer> trainingID=new ArrayList<>();
         return trainingID;
